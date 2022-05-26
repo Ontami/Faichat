d70714640b2cd608749f96b101c192a6b00f29d3
ONTAMI STUDIO <99495587+Ontami@users.noreply.github.com>
2022-05-26 15:17:08 +0200

    Add files via upload

---
+++ faichat.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta name="author" content="NoS1gnal"/>
+            <link rel="shortcut icon" href="./assets/Faichat.png">
+              <link rel="stylesheet" href="./css/Style.css">
+            <title>FAICHAT | Connexion</title>
+        </head>
+        <body class="bodycolor">
+        
+        <div class="">
+             <?php 
+             
+             ?>
+               
+        </style>
+        </body>
+</html>
